e3f8744ab0a6d2a5d1ae860abac7a658681f160c
Thanh Le <thanhlevan0203@gmail.com>
2022-09-25 22:22:45 -0700

    Create Top section Landing Page

--- car-rental-app/src/app/containers/HomePage/topSection.tsx
+++ car-rental-app/src/app/containers/HomePage/topSection.tsx
@@ -32,9 +32,26 @@ const RightContainer = styled.div`
         relative
         mt-20
     `};
-
-    
 `;
+
+const Slogan = styled.h1`
+   ${tw`
+        font-bold
+        text-2xl 
+        xl:text-6xl 
+        sm:text-3xl 
+        md:text-5xl 
+        lg:font-black 
+        md:font-extrabold 
+        text-black 
+        mb-4 
+        sm:leading-snug 
+        lg:leading-normal 
+        xl:leading-relaxed 
+   `} 
+`;
+
+const Description = styled.p
 
 export function TopSection() {
 
